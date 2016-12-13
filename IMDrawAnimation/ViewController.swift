2c2007124d754cede7238c83cd62145276538f76
LeeWorker <lsh249410504@163.com>
2016-12-13 14:38:59 +0800

    Initial Commit

---
+++ IMDrawAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMDrawAnimation
+//
+//  Created by imwallet on 16/12/13.
+//  Copyright © 2016年 imWallet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
